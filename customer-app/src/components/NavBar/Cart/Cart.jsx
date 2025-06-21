9a008e2e82bfdd311849ab2cb7be86fd3f9ebe37
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-06-21 16:05:24 +0200

    refactor: - improved Cart component structure and styling - price in Cart is now always rounded to 2 decimals after coma (e.g. "10.91")

--- customer-app/src/components/NavBar/Cart/Cart.jsx
+++ customer-app/src/components/NavBar/Cart/Cart.jsx
@@ -1,19 +1,27 @@
-import { useSelector } from "react-redux";
-import CartIcon from "@icons/cart.svg?react"
-
 import styles from "./Cart.module.scss";
 
-export default function Cart(){
+import { useSelector } from "react-redux";
+import CartIcon from "@icons/cart.svg?react";
+
+export default function Cart() {
     const itemsInCartLenght = useSelector((state) => state.cart.items.length);
-    const itemsInCartPrice = useSelector((state) => state.cart.items.reduce((acc, item) => acc + item.price, 0));
+    const itemsInCartPrice = useSelector((state) =>
+        state.cart.items.reduce((acc, item) => acc + item.price, 0)
+    );
 
     return (
         <div className={styles.cart_container}>
             <div className={styles.cart_icon_container}>
-                <div><CartIcon/></div>
-                {itemsInCartLenght > 0 && <div className={styles.cart_items_counter}>{itemsInCartLenght}</div>}
+                <CartIcon className={styles.cart_icon} />
+                {itemsInCartLenght > 0 && (
+                    <div className={styles.cart_items_counter}>
+                        {itemsInCartLenght}
+                    </div>
+                )}
             </div>
-            <div className={styles.cart_price}>{itemsInCartPrice} zł</div>
+            <div className={styles.cart_price}>
+                {itemsInCartPrice.toFixed(2)} zł
+            </div>
         </div>
     );
 }
