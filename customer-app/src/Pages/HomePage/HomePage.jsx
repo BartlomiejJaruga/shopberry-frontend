9081f4b23b573e1c24f3803ff0c7762199557de5
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-06-21 17:29:39 +0200

    feat: - implement category fetching and loading state in HomePage component - add categories slice for managing main and subcategories in Redux store

--- customer-app/src/Pages/HomePage/HomePage.jsx
+++ customer-app/src/Pages/HomePage/HomePage.jsx
@@ -1,15 +1,70 @@
 import styles from "@pages/HomePage/HomePage.module.scss";
 
 import NavBar from "@components/NavBar/NavBar";
+import axiosInstance from "@services/axiosInstance";
+import { useEffect, useState } from "react";
+import LoadingIndicator from "@components/LoadingIndicator/LoadingIndicator";
+import { useDispatch } from "react-redux";
+import {
+    updateMainCategories,
+    updateCategories,
+} from "@slices/categoriesSlice";
 
 export default function HomePage() {
+    const dispatch = useDispatch();
+    const [isPageGettingLoaded, setIsPageGettingLoaded] = useState(true);
+
+    const getAllCategoriesFromDatabase = async () => {
+        setIsPageGettingLoaded(true);
+
+        try {
+            const response = await axiosInstance.get("/v1/categories");
+
+            const { mainCategories, categories } = response.data.reduce(
+                (acc, category) => {
+                    if (category.parent_category === null) {
+                        acc.mainCategories.push(category);
+                    } else {
+                        acc.categories.push(category);
+                    }
+                    return acc;
+                },
+                { mainCategories: [], categories: [] }
+            );
+
+            dispatch(updateMainCategories({ mainCategories: mainCategories }));
+            dispatch(updateCategories({ categories: categories }));
+        } catch (error) {
+            console.error(error);
+        }
+
+        setIsPageGettingLoaded(false);
+    };
+
+    useEffect(() => {
+        getAllCategoriesFromDatabase();
+    }, []);
+
     return (
         <>
-            <NavBar />
-            <div className={styles.homepage_container}>
-                <img src="/logo.png" alt="ShopBerry Logo" />
-                <h1>Welcome to ShopBerry Page!</h1>
-            </div>
+            {isPageGettingLoaded && (
+                <>
+                    <LoadingIndicator
+                        message="Page is being loaded..."
+                        fontSize="3rem"
+                    />
+                </>
+            )}
+
+            {!isPageGettingLoaded && (
+                <>
+                    <NavBar />
+                    <div className={styles.homepage_container}>
+                        <img src="/logo.png" alt="ShopBerry Logo" />
+                        <h1>Welcome to ShopBerry Page!</h1>
+                    </div>
+                </>
+            )}
         </>
     );
 }
--- customer-app/src/store/index.js
+++ customer-app/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "@slices/cartSlice";
 import authReducer from "@slices/authSlice";
+import categoriesReducer from "@slices/categoriesSlice";
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
         auth: authReducer,
+        categories: categoriesReducer,
     },
 });
