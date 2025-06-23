ca4e8e30663b08a99ecae0966e64da917ea8f8a4
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-06-23 16:40:45 +0200

    fix: - update useEffect dependencies to include searchTerm

--- customer-app/src/Pages/CategoryPage/CategoryPage.jsx
+++ customer-app/src/Pages/CategoryPage/CategoryPage.jsx
@@ -64,7 +64,7 @@ export default function CategoryPage() {
         } else {
             getAllCategoryProductsFromDatabase();
         }
-    }, [categoryId]);
+    }, [categoryId, searchTerm]);
 
     return (
         <>
